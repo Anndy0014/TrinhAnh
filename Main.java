1e7ed46c65ac981c91b990da9b4d1b5fe77872f8
TrinhAnh <B16013@nishitan.jp>
2020-07-02 20:12:35 +0900

    add Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    public static void addStudent(){
+
+    }
+    public static void editStudent(){
+
+    }
+    public static void deleteStudent(){
+
+    }
+    public static void findStudent(){
+
+    }
+    public static void Student(){
+
+    }
+}
